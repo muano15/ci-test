862e6b6c5f34b0e3604368e60794bb03b70e53e6
Muano <muano.m365@gmail.com>
2023-05-05 22:04:47 +0200

    added more tests

--- app.test.js
+++ app.test.js
@@ -2,8 +2,10 @@ const app = require('./app')
 
 test("properly adds two numbers", () => {
     expect(app.Sum(1, 2)).toBe(3)
+    expect(app.Sum(4, 7)).not.toBe(11)
 })
 
 test("properly subtracts two numbers", () => {
     expect(app.Subtract(1, 2)).toBe(-1)
+    expect(app.Subtract(9, 2)).not.toBe(7)
 })
